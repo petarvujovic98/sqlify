9e4ef640464c58607eb1eef27c130c87c620f0c2
Petar Vujović <petarvujovic98@gmail.com>
2023-07-03 12:15:53 +0200

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# sqlify
+
+A CLI tool to format your SQL code.
+
+## Features
+
+This tool utilizes [sqlformat](https://github.com/shssoichiro/sqlformat-rs)
+under the hood to format the given SQL code.
+
+## Usage
+
+To use it simply pass in your SQL as the argument, or pipe it from a file, like this:
+
+```sh
+sqlify "SELECT * FROM tablename"
+
+# or
+
+cat query.sql | sqlify
+
+# or
+
+sqlify < query.sql
+```
+
+where `query.sql` looks like
+
+```sql
+SELECT * FROM tablename
+```
+
+to get the following output:
+
+```sql
+SELECT
+  *
+FROM
+  tablename
+```
+
+There are fomrating options from [sqlformat](https://docs.rs/sqlformat/latest/sqlformat/struct.FormatOptions.html)
+exposed through arguments:
+
+- `-u` or `--uppercase` to denote the use of uppercase keywords
+- `-t` or `--tabs` to denote the use of tabs
+- `-i` or `--indent` to specify the number of spaces to use for indentation
+  (defaults to 2), ignored if tabs are used
+- `-l` or `--lines-between-queries` to specify the number of line breaks
+  to use between queries (defaults to 1)
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+#![forbid(unsafe_code)]
+#![warn(clippy::all, clippy::pedantic)]
+#![doc = include_str!("../README.md")]
+
+use std::io::Read;
+
+use clap::Parser;
+
+/// CLI tool to format your SQL code.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The SQL code to format. Could be passed as an argument or piped through stdin.
+    sql: Option<String>,
+
+    /// Whether to use uppercase keywords.
+    #[clap(short, long)]
+    uppercase: bool,
+
+    /// Whether to use tabs instead of spaces.
+    #[clap(short, long)]
+    tabs: bool,
+
+    /// The number of spaces to use for indentation.
+    #[clap(short, long, default_value = "2")]
+    indent: u8,
+
+    /// The number of lines between queries.
+    #[clap(short, long, default_value = "1")]
+    lines_between_queries: u8,
+}
+
+impl From<&Args> for sqlformat::FormatOptions {
+    fn from(args: &Args) -> Self {
+        Self {
+            indent: if args.tabs {
+                sqlformat::Indent::Tabs
+            } else {
+                sqlformat::Indent::Spaces(args.indent)
+            },
+            uppercase: args.uppercase,
+            lines_between_queries: args.lines_between_queries,
+        }
+    }
+}
+
+impl Args {
+    /// Format the SQL code.
+    fn format(&self) -> String {
+        let sql = self.get_sql();
+        let indentation = sql.len() - sql.trim_start().len();
+        let formatted = sqlformat::format(&sql, &sqlformat::QueryParams::None, self.into());
+        formatted
+            .lines()
+            .map(|line| {
+                format!(
+                    "{}{line}",
+                    (0..indentation).map(|_| " ").collect::<Vec<_>>().join("")
+                )
+            })
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    /// Get the SQL code from stdin or the command line.
+    fn get_sql(&self) -> String {
+        if let Some(sql) = &self.sql {
+            sql.clone()
+        } else {
+            let mut sql = String::new();
+            std::io::stdin()
+                .read_to_string(&mut sql)
+                .expect("Failed to read from stdin");
+            sql
+        }
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("{}", args.format());
+}
